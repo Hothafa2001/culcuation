d54d98a4586f2fa36fe04425c6ab42397144c304
Hothafa2001 <144283798+Hothafa2001@users.noreply.github.com>
2023-09-06 14:27:24 -0700

    Add files via upload

---
+++ الحاسبة/js.js
@@ -0,0 +1,8 @@
+const input = document.getElementById('input');
+const result = document.getElementById('result');
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.id === '=')
+  }
+}
